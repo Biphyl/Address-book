5f6a0a0faaff75668f99c1ce3461692093513748
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-02-26 15:53:02 +0300

    added jQuery function in user interface logic side

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,26 @@ function contact(first, last) {
 }
 
 
+// user interface logic
+$(document).ready(function(){
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact= newContact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span><li>");
+
+    $("input#new-first-name").val("");
+    $("iput#new-last-name").val("");
+  });
+
+  $(".contact").last().click(function(){
+    $("#show-contact").show();
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+  })
+});
